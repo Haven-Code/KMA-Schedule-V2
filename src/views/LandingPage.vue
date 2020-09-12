fa5a236730cb9ada72ccd25fc722ed1e3896ff57
Dang Phuc <phuc.px.371@gmail.com>
2020-09-12 23:27:21 +0700

    fix blink animation

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -64,7 +64,7 @@
 		text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
 	}
 
-	@-webkit-keyframes glow {
+	@keyframes glow {
 		from {
 			text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
 		}
@@ -94,7 +94,8 @@
 	}
 
 	.flicker {
-		-webkit-animation: flickerAni 1.5s infinite;
+		-webkit-animation: flickerAni 2s infinite;
+		animation: flickerAni 2s infinite;
 	}
 
 	.bg {
